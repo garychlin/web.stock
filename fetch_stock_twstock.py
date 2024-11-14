b86f2cfc30c6392fff4ccd99e201a1389c9505a0
garylin <garylin@hemidemi.com>
2024-11-14 15:10:00 +0800

    fix fetch_stock_twstock bug

--- fetch_stock_twstock.py
+++ fetch_stock_twstock.py
@@ -3,16 +3,30 @@ import twstock
 import json
 
 # 獲取股票代號
-stock_codes = sys.argv[1:]  # 從命令行參數獲取多個股票代號
+if len(sys.argv) > 1:
+    stock_code = sys.argv[1]
+else:
+    stock_code = '2330'  # 默認為台積電的股票代號
 
-# 獲取股票資料
-prices = {}
-for stock_code in stock_codes:
-    try:
-        stock = twstock.Stock(stock_code)
-        prices[stock_code] = stock.price[-1] if stock.price else None  # 獲取最新股價
-    except Exception as e:
-        prices[stock_code] = str(e)  # 返回錯誤信息
+try:
+    # 獲取股票資料
+    stock = twstock.Stock(stock_code)
 
-# 返回 JSON 格式的數據
-print(json.dumps(prices))  # 輸出 JSON 數據
+    # 獲取即時股價
+    price = stock.price
+
+    # 返回 JSON 格式的數據
+    data = {
+        'stock_code': stock_code,
+        'price': price
+    }
+except Exception as e:
+    # 返回錯誤信息
+    data = {
+        'error': str(e)
+    }
+
+# 确保只输出 JSON 数据，没有其他输出
+print(json.dumps(data))  # 輸出 JSON 數據
+
+
